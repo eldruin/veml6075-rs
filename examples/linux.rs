82801da39a483ef9efde8c75ea501a2d4b2a8a6a
Diego Barrios Romero <eldruin@gmail.com>
2019-04-01 19:14:18 +0200

    Simplify example code

--- examples/linux.rs
+++ examples/linux.rs
@@ -1,12 +1,9 @@
-extern crate embedded_hal;
-extern crate linux_embedded_hal;
+extern crate linux_embedded_hal as hal;
 extern crate veml6075;
-
-use linux_embedded_hal::I2cdev;
 use veml6075::Veml6075;
 
 fn main() {
-    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
     let mut sensor = Veml6075::new(dev);
     let uva = sensor.read_uva().unwrap();
     let uvb = sensor.read_uvb().unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -39,16 +39,14 @@
 //! ```no_run
 //! extern crate linux_embedded_hal as hal;
 //! extern crate veml6075;
-//!
-//! use hal::I2cdev;
 //! use veml6075::Veml6075;
 //!
 //! # fn main() {
-//! let dev = I2cdev::new("/dev/i2c-1").unwrap();
-//! let mut uv_light_sensor = Veml6075::new(dev);
-//! uv_light_sensor.enable().unwrap();
-//! let _uva_reading = uv_light_sensor.read_uva().unwrap();
-//! let _uvb_reading = uv_light_sensor.read_uvb().unwrap();
+//! let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
+//! let mut sensor = Veml6075::new(dev);
+//! let uva = sensor.read_uva().unwrap();
+//! let uvb = sensor.read_uvb().unwrap();
+//! println!("Measurements UVA: {}, UVB: {}", uva, uvb);
 //! # }
 //! ```
 
